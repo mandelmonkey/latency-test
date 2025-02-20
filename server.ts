7a4ea62e73c4984c0e29014935fdf5f86f82bcc0
mandelmonkey <chrismoss411@gmail.com>
2025-02-20 12:00:54 +0000

    am

--- server.ts
+++ server.ts
@@ -102,7 +102,7 @@ function createApp() {
     // List of remote endpoints to test
     const endpoints = [
       { name: "US", url: "https://latency-test-us-dc1c0df1e579.herokuapp.com" },
-      { name: "EU", url: "https://latency-test-eu-6615850a4a65.herokuapp.com" }
+      { name: "EU", url: "https://latency-test-eu-6615850a4a65.herokuapp.com" },
       { name: "EU-EAST", url: "https://latency-test-us-east.onrender.com" }
     ];
 
